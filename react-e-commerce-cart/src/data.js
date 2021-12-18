f2cf34f896356fb8d109035b6410aed692216464
Prithiviraj69 <85940424+Prithiviraj69@users.noreply.github.com>
2021-12-18 19:52:18 +0530

    Add files via upload

---
+++ react-e-commerce-cart/src/data.js
@@ -0,0 +1,40 @@
+const data = {
+    products: [{
+            id: "1",
+            name: "BROTHERHOOD ONE PIECE pvc action figure toys anime figure",
+            price: 2000,
+            image: "https://s.alicdn.com/@sc04/kf/U11f1361a14ae43878c432970a252c6cc7.jpg_220x220.jpg"
+        },
+        {
+            id: "2",
+            name: "Dragon Ball Z",
+            price: 2207,
+            image: "	https://s.alicdn.com/@sc04/kf/H5f5795d24f274646a2d5b9ea07159d00S.jpg_220x220.jpg"
+        },
+        {
+            id: "3",
+            name: "Attack on Titan ",
+            price: 1988,
+            image: "https://s.alicdn.com/@sc04/kf/Hc60b7bf1f72e40d98695ddd3acd58680f.jpg_220x220.jpg"
+        },
+        {
+            id: "4",
+            name: "Naruto(NineTales)",
+            price: 3000,
+            image: "https://s.alicdn.com/@sc04/kf/H0ee15768bd07481dab0e36143eb78c85u.jpg_220x220.jpg"
+        },
+        {
+            id: "5",
+            name: "Broly",
+            price: 3300,
+            image: "https://s.alicdn.com/@sc04/kf/H9a788bbf9f3c436c9f2c4ee3a575e06eD.jpg_220x220.jpg"
+        },
+        {
+            id: "6",
+            name: "Ninja Turtles",
+            price: 1100,
+            image: "https://s.alicdn.com/@sc04/kf/Hfc27fb4d6192478c87f5847f6d4e5264e.jpg_220x220.jpg"
+        }
+    ]
+};
+export default data;
